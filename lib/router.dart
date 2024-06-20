7a24fe92f69b916d8b01dca55a7fc6e9ac34715d
Shri333 <dshrihan@gmail.com>
2024-06-19 20:06:18 -0400

    rename screens -> widgets

--- lib/router.dart
+++ lib/router.dart
@@ -1,5 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:photofeast/screens/home.dart';
+import 'package:photofeast/widgets/home.dart';
 
 final router = GoRouter(routes: [
   GoRoute(
